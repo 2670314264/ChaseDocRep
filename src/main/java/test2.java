70814af1a4ce9df22374e5d8e13e0a7b5bf73dda
snoop <2670314264@xx.com>
2022-03-08 22:22:22 +0800

    add test2

---
+++ src/main/java/test2.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName test2
+ * @Date 2022/3/8 22:20
+ * @Author Chase.Li
+ * @Description
+ */
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("this is test2");
+    }
+}
